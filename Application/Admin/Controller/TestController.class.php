6936d6a19b3c4c8a3d9517b51e20ac66f2afd955
HolmesWu <wuyueholmes@qq.com>
2015-09-24 19:18:22 +0800

    改了些东西

--- Application/Admin/Controller/TestController.class.php
+++ Application/Admin/Controller/TestController.class.php
@@ -4,6 +4,31 @@ use Think\Controller;
 class TestController extends BaseController {
     public function index(){
     	$this->initialize('题目列表');
+    	$this->assign('questions',D('questions')->select());
     	$this->display();
+    }
+    public function ueditor(){
+        $data = new \Org\Util\Ueditor();
+        echo $data->output();
+    }
+    public function delete(){
+    	if(IS_GET){
+    		if($id=I('get.questionid')){
+    			$result=D('questions')->delete($id);
+    			$this->ajaxReturn($result);
+    		}
+    	}else{
+    		$this->ajaxReturn('wrong');
+    		redirect(U('test/index'));
+    	}
+    }
+    public function update(){
+    	if(IS_POST){
+    		if($id=I('post.questionid')){
+    			$this->ajaxReturn(D('questions')->delete($id));
+    		}
+    	}else{
+    		redirect(U('test/index'));
+    	}
     }
 }
